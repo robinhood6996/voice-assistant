da0863e43c87bbacd3aeda6bf583402037f4e17e
Fakrul Islam Robin <fakrulrobin7@gmail.com>
2024-07-02 01:01:33 +0600

    dummy msg added

---
+++ constants/chats.ts
@@ -0,0 +1,6 @@
+export const dummyMsgs = [
+  { role: "user", content: "How are you ?" },
+  { role: "assistant", content: "I'm fine. How may I help you today ?" },
+  { role: "user", content: "Create an image with dog playing ball" },
+  { role: "assistant", content: "Dummy image" },
+];
